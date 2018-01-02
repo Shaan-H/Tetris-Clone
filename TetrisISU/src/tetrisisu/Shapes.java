f189db64179d0d16a41e551671c94167dbc220cc
skelebro <35014078+skelebro@users.noreply.github.com>
2018-01-01 20:00:02 -0500

    lol

---
+++ TetrisISU/src/tetrisisu/Shapes.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetrisisu;
+
+/**
+ *
+ * @author Skelebro
+ */
+public enum Shapes {
+    
+    
+ 
+
+ 
+  private Shapes(int[][] coords, Color c) {
+    this.coords = coords;
+    color = c;
+    
+    public int[][] coords;
+    public Color color;
+
+    ZShape(new int[][] { { 0, -1 }, { 0, 0 }, { -1, 0 }, { -1, 1 } }, new Color(204, 102, 102)),
+    SShape(new int[][] { { 0, -1 }, { 0, 0 }, { 1, 0 }, { 1, 1 } }, new Color(102, 204, 102)),
+    LineShape(new int[][] { { 0, -1 }, { 0, 0 }, { 0, 1 }, { 0, 2 } }, new Color(102, 102, 204)),
+    TShape(new int[][] { { -1, 0 }, { 0, 0 }, { 1, 0 }, { 0, 1 } }, new Color(204, 204, 102)),
+    SquareShape(new int[][] { { 0, 0 }, { 1, 0 }, { 0, 1 }, { 1, 1 } }, new Color(204, 102, 204)),
+    LShape(new int[][] { { -1, -1 }, { 0, -1 }, { 0, 0 }, { 0, 1 } }, new Color(102, 204, 204)),
+    MirroredLShape(new int[][] { { 1, -1 }, { 0, -1 }, { 0, 0 }, { 0, 1 } }, new Color(218, 170, 0));
+  }
+
+}
