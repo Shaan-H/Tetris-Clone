a1781710182b188a1d41dcbb2c59b058e374f89e
skelebro <35014078+skelebro@users.noreply.github.com>
2018-01-02 17:43:38 -0500

    KK

--- TetrisISU/src/tetrisisu/Shapes.java
+++ TetrisISU/src/tetrisisu/Shapes.java
@@ -46,7 +46,8 @@ public class Shapes {
             { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
             { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
             { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) }
-=======
+        },
+        
         //i piece
         {
             {new Point(0,0), new Point(0,1), new Point(0,2), new Point(0,3)},
