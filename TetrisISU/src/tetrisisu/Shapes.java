0458e57cea6225501596ef73201913d527d761c2
Shaan-H <buffalopaladin@gmail.com>
2018-01-02 15:48:17 -0500

    erm

--- TetrisISU/src/tetrisisu/Shapes.java
+++ TetrisISU/src/tetrisisu/Shapes.java
@@ -5,30 +5,65 @@
  */
 package tetrisisu;
 
+import java.awt.Color;
+import static java.awt.Color.*;
+import java.awt.Point;
+
 /**
  *
- * @author Skelebro
+ * @author Admin
  */
-public enum Shapes {
-    
-    
- 
-
- 
-  private Shapes(int[][] coords, Color c) {
-    this.coords = coords;
-    color = c;
-    
-    public int[][] coords;
-    public Color color;
-
-    ZShape(new int[][] { { 0, -1 }, { 0, 0 }, { -1, 0 }, { -1, 1 } }, new Color(204, 102, 102)),
-    SShape(new int[][] { { 0, -1 }, { 0, 0 }, { 1, 0 }, { 1, 1 } }, new Color(102, 204, 102)),
-    LineShape(new int[][] { { 0, -1 }, { 0, 0 }, { 0, 1 }, { 0, 2 } }, new Color(102, 102, 204)),
-    TShape(new int[][] { { -1, 0 }, { 0, 0 }, { 1, 0 }, { 0, 1 } }, new Color(204, 204, 102)),
-    SquareShape(new int[][] { { 0, 0 }, { 1, 0 }, { 0, 1 }, { 1, 1 } }, new Color(204, 102, 204)),
-    LShape(new int[][] { { -1, -1 }, { 0, -1 }, { 0, 0 }, { 0, 1 } }, new Color(102, 204, 204)),
-    MirroredLShape(new int[][] { { 1, -1 }, { 0, -1 }, { 0, 0 }, { 0, 1 } }, new Color(218, 170, 0));
-  }
-
+public class Shapes {
+    public final Point[][][] TetrisShapes = {
+        //i piece
+        {
+            {new Point(0,0), new Point(0,1), new Point(0,2), new Point(0,3)},
+            {new Point(0,0), new Point(1,0), new Point(2,0), new Point(3,0)},
+            {new Point(0,0), new Point(0,1), new Point(0,2), new Point(0,3)},
+            {new Point(0,0), new Point(1,0), new Point(2,0), new Point(3,0)},
+        },
+        //j piece
+        {
+            {new Point(0,-1), new Point(0,0), new Point(0,1), new Point(0,2)},
+            {new Point(0,1), new Point(0,0), new Point(1,0), new Point(2,0)},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+        },
+        //L piece
+        {
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+        },
+        //O piece
+        {
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+        },
+        //S piece
+        {
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+        },
+        //T piece
+        {
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+        },
+        //Z piece
+        {
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+            {new Point(), new Point(), new Point(), new Point()},
+        },
+    };
+    public Color[] ShapeColor = {cyan, blue, orange,yellow, green, pink,red};
 }
